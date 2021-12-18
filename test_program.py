29ecdeb30f09ac8aec4b46043ceac7dc2da487c9
egorovmaxim <96294602+egorovmaxim@users.noreply.github.com>
2021-12-18 15:09:12 +0300

    вернул тест к валидному состоянию

--- test_program.py
+++ test_program.py
@@ -2,7 +2,7 @@ import program
 
 # обязательный префикс 'test' в названии функции
 def test_program():
-    assert program.min([1, 2, -5, 6]) == -4, "Тест не прошёл для функции min"
+    assert program.min([1, 2, -5, 6]) == -5, "Тест не прошёл для функции min"
     assert program.max([-9, -1, 3, 0, 0]) == 3, "Тест не прошёл для функции max"
     assert program.sum([2, 1, 2, 3, 1]) == 9, "Тест не прошёл для функции sum"
     assert program.mult([2, 2, 3, 1, 1, 2]) == 24, "Тест не прошёл для функции mult"
